c0359a8aecaf350f7577f1f7edecb4fc885c40f2
Keiko Nakata <kk.nkt33@gmail.com>
2022-02-14 16:44:25 +0100

    concurrent stack

---
+++ datastructures/pstack.hpp
@@ -0,0 +1,68 @@
+#include <condition_variable>
+#include <exception>
+#include <iostream>
+#include <memory>
+#include <mutex>
+#include <thread>
+#include <utility>
+#include "myitem.hpp"
+
+template <typename T>
+class Stack {
+  struct Item {
+    using next_ptr = std::unique_ptr<Item>;
+    T data;
+    next_ptr next;
+    Item(const T& _data, next_ptr _next) : data(_data), next(std::move(_next)) {};
+    Item(T&& _data, next_ptr _next) : data(std::move(_data)), next(std::move(_next)) {};
+    Item(const T& _data) : data(_data), next(nullptr) {}
+  };
+  std::unique_ptr<Item> head;
+  mutable std::mutex m;
+  std::condition_variable cv;
+public:
+  Stack() : head(nullptr) {}
+  ~Stack() {
+    while (head) {
+      head = std::move(head->next);
+    }
+  }
+  void push(const T& v) {
+    // Create a new node before acquiring the lock
+    auto new_head = std::make_unique<Item>(v);
+    const std::lock_guard<std::mutex> lock(m);
+    // These assignments are noexcept
+    new_head->next = std::move(head);
+    head = std::move(new_head);
+    cv.notify_one();
+  }
+  std::unique_ptr<T> pop() {
+    const std::lock_guard<std::mutex> lock(m);
+    if (!head)
+      return nullptr;
+    auto ret = std::unique_ptr<T>(new T{head->data});
+    head = std::move(head->next);
+    return ret;
+  }
+  bool pop(T& ret) {
+    const std::lock_guard<std::mutex> lock(m);
+    if (!head)
+      return false;
+    ret = head->data;
+    head = std::move(head->next);
+    return true;
+  }
+  void pop_await(T& ret) {
+    std::unique_lock<std::mutex> lock(m);
+    cv.wait(lock, [this]{return head != nullptr;});
+    ret = head->data;
+    head = std::move(head->next);
+  }
+  std::unique_ptr<T> pop_await() {
+    std::unique_lock<std::mutex> lock(m);
+    cv.wait(lock, [this]{return head != nullptr;});
+    auto ret = std::unique_ptr<T>(new T{head->data});
+    head = std::move(head->next);
+    return ret;
+  }
+};
