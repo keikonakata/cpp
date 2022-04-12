39b9fd64b6598baf6b6c548e4841149f5e1c308f
Keiko Nakata <keko.nakata@gmail.com>
2022-04-12 23:56:44 +0200

    bug fix tarjans_norec

--- datastructures/tarjans/tarjans_main.cc
+++ datastructures/tarjans/tarjans_main.cc
@@ -2,6 +2,7 @@
 // clang++-10 -Wall -std=c++17 tarjans.o tarjans_main.cc
 #include <cassert>
 #include "tarjans.h"
+#include "tarjans_norec.h"
 
 int main(int argc, char* argv[]){
   using namespace tarjans;
@@ -14,7 +15,9 @@ int main(int argc, char* argv[]){
       {5, {}},
     };
     Tarjans t (g);
+    tarjans_norec::Tarjans t2 (g);
     assert(t.components.size() == 5);
+    assert(t2.components.size() == 5);
   }
 
   {
@@ -26,9 +29,10 @@ int main(int argc, char* argv[]){
       {5, {5}},
     };
     Tarjans t (g);
+    tarjans_norec::Tarjans t2 (g);
     assert(t.components.size() == 5);
+    assert(t2.components.size() == 5);
   }
-
   {
     graph_t const g = {
       {1, {2}},
@@ -38,9 +42,10 @@ int main(int argc, char* argv[]){
       {5, {1}},
     };
     Tarjans t (g);
+    tarjans_norec::Tarjans t2 (g);
     assert(t.components.size() == 1);
+    assert(t2.components.size() == 1);
   }
-
   {
     graph_t const g = {
       {1, {2}},
@@ -50,13 +55,16 @@ int main(int argc, char* argv[]){
       {5, {3, 1}},
     };
     Tarjans t (g);
+    tarjans_norec::Tarjans t2 (g);
     assert(t.components.size() == 3);
+    assert(t2.components.size() == 3);
   }
 
   {
     graph_t const g = {{1, {2}}};
     try {
       Tarjans t (g);
+      tarjans_norec::Tarjans t2 (g);
     }
     catch (const std::invalid_argument& e)
       {}
--- datastructures/tarjans/tarjans_norec.cc
+++ datastructures/tarjans/tarjans_norec.cc
@@ -16,10 +16,10 @@ void Tarjans::tarjans_loop(vertex_t root, const std::vector<vertex_t>& targets){
   while (!stack.empty()){
     auto& t = stack.back();
     auto const vertex = std::get<0>(t);
+    auto const index = indexes.at(vertex);
     if (std::get<1>(t) == std::get<2>(t)){
       // the vertex is black
       stack.pop_back();
-      auto const index = indexes.at(vertex);
       auto const lowestlink = lowestlinks.at(vertex);
       if (lowestlink == index){
 	// the vertex is the root of a SCC
@@ -34,20 +34,34 @@ void Tarjans::tarjans_loop(vertex_t root, const std::vector<vertex_t>& targets){
     } else {
       // the vertex is gray
       vertex_t const target = *std::get<1>(t);
-      std::advance(std::get<1>(t), 1);
       if (auto const target_index = indexes.find(target); target_index == indexes.end()){
 	// target is white
 	auto const new_edges = graph.find(target);
 	if (new_edges == graph.end())
 	  throw std::invalid_argument("vertex not found in the graph");
+	indexes.emplace(target, next_index);
+	lowestlinks.emplace(target, next_index);
+	path.insert({next_index, target});
 	stack.emplace_back(target, new_edges->second.cbegin(), new_edges->second.cend());
+	++next_index;
       } else {
 	// target is grey or black
-	if (path.find(target_index->second) != path.end()) {
-	  // vertex |-> target is a back-edge
+	std::advance(std::get<1>(t), 1);
+	auto const target_index_value = target_index->second;
+	if (target_index_value < index) {
+	  // vertex |-> target is either cross-edge or back-edge
+	  if (path.find(target_index_value) != path.end()) {
+	    // vertex |-> target is a back-edge
+	    auto const current_lowestlink = lowestlinks.at(vertex);
+	    if (target_index_value < current_lowestlink)
+	      lowestlinks.insert_or_assign(vertex, target_index->second);
+	  }
+	} else {
+	  // vertex |-> target is tree-edge or forward edge
 	  auto const current_lowestlink = lowestlinks.at(vertex);
-	  if (target_index->second < current_lowestlink)
-	    lowestlinks.insert({vertex, target_index->second});
+	  auto const child_lowestlink = lowestlinks.at(target);
+	  if (child_lowestlink < current_lowestlink)
+	    lowestlinks.insert_or_assign(vertex, child_lowestlink);
 	}
       }
     }
